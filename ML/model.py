32046ca8168c5c18938711476db8b01f52609938
Marek Gajewski <marek.gajewski2001@gmail.com>
2024-01-14 20:47:18 +0100

    mapper reducer

--- ML/model.py
+++ ML/model.py
@@ -24,3 +24,17 @@ def average_parameters(models):
                 averaged_params[k] += v / num_models
 
     return averaged_params
+
+def mapper_train(model, data_subset):
+    for data in data_subset:
+        # Update model with data
+        model.train(data)
+    return model
+
+def reducer_aggregate(models):
+    # Aggregate models using a chosen method
+    averaged_params = average_parameters(models)
+    aggregated_model = MyModel()
+    aggregated_model.set_parameters(averaged_params)
+    return aggregated_model
+
